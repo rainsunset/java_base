3b98b78e932363db7aba42c95ff13dc444cef4da
ligangwei <ligangwei@cmbi.com.hk>
2021-04-30 18:03:47 +0800

    tcp

--- src/main/java/com/ligw/javabase/first/TypeCovTest.java
+++ src/main/java/com/ligw/javabase/first/TypeCovTest.java
@@ -6,9 +6,15 @@ package com.ligw.javabase.first;
 
 /**
  * 数据类型转换 Type conversion
+ *
  * @author ligw
  * @version $Id TypeCovTest.java, v 0.1 2018-03-22 15:34 ligw Exp $$
  */
 public class TypeCovTest {
-
+    public static void main(String[] args) {
+        double a = 32767;
+        a ++;
+        System.out.println(a);
+        a++;
+    }
 }
